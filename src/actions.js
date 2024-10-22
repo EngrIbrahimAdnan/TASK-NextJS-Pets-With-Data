4158d6b7f3084886234205d9213ee51a167034bc
ibrahim.a.y.m.98@gmail.com <ibrahim.a.y.m.98@gmail.com>
2024-10-22 15:24:00 +0300

    done 4 task

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,12 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+
+const headers = new Headers();
+headers.append("Content-Type", "application/json");
+
+const baseURL = "https://pets-react-query-backend.eapi.joincoded.com";
 
 export async function fetchingPets() {
   const response = await fetch(
@@ -24,4 +30,22 @@ export async function fetchingPetDetail(id) {
   }
 
   return pet;
+}
+
+export async function addPet(formData) {
+  const petData = {
+    ...Object.fromEntries(formData),
+    adopted: 0,
+  };
+
+  const response = await fetch(`${`${baseURL}/pets`}`, {
+    method: "POST",
+    headers,
+    body: JSON.stringify(petData),
+  });
+
+  const newPet = await response.json();
+  redirect("/pets");
+
+  return newPet;
 }
